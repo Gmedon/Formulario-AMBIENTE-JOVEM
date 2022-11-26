96c2ed3c6ec70456e570a113883ee0071118281d
Guilherme Medon <113711968+Gmedon@users.noreply.github.com>
2022-11-26 20:13:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let verificarheight = window.screen.height;
 let main = document.querySelector('main')
-main.style.height = verificarheight + "px";
+main.style.height = (verificarheight + 150) + "px";
 let inputs = document.querySelectorAll('.input-info')
 let bt = document.querySelector('.bt')
 bt.addEventListener("click", function (event) {
@@ -8,17 +8,21 @@ bt.addEventListener("click", function (event) {
         event.preventDefault()
         inputs[0].style.border = "2px solid red";
         setTimeout('inputs[0].style.border = "2px solid #999"', 2000)
+        scrollTo(0, 0)
     } else if (inputs[1].value == "") {
         event.preventDefault()
         inputs[1].style.border = "2px solid red";
         setTimeout('inputs[1].style.border = "2px solid #999"', 2000)
+        scrollTo(0, 0)
     } else if (inputs[2].value == "") {
         event.preventDefault()
         inputs[2].style.border = "2px solid red";
         setTimeout('inputs[2].style.border = "2px solid #999"', 2000)
+        scrollTo(0, 0)
     } else if (inputs[3].value == "") {
         event.preventDefault()
         inputs[3].style.border = "2px solid red";
         setTimeout('inputs[3].style.border = "2px solid #999"', 2000)
+        scrollTo(0, 0)
     }
 });
