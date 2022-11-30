a696613eedfaba6dda41ce58e8f83e5cbad005f2
Guilherme Medon <113711968+Gmedon@users.noreply.github.com>
2022-11-30 14:57:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let inputs = document.querySelectorAll('.input-info')
+function enviarform() {
+    if (inputs[0].value == "") {
+        alert("Preencha todos os campos pessoais.")
+    } else if (inputs[1].value == "") {
+        alert("Preencha todos os campos pessoais.")
+    } else if (inputs[2].value == "") {
+        alert("Preencha todos os campos pessoais.")
+    } else if (inputs[3].value == "") {
+        alert("Preencha todos os campos pessoais.")
+    } else if (inputs[3].value == "") {
+        alert("Preencha todos os campos pessoais.")
+    }
+}
+let bt = document.querySelector("#limpar")
+bt.addEventListener("click", function (e) {
+    e.preventDefault()
+    inputs[0].setAttribute('value', "")
+});
